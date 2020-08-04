839ca7b14a7b1f9a2ee49eecb3cb6a7fcbbd9ab4
yuhongzhou95 <yuhongzhou95@vip.qq.com>
2020-08-04 12:37:09 +0800

    二分查找代码优化

--- src/com/shadow/BinarySearch.java
+++ src/com/shadow/BinarySearch.java
@@ -8,7 +8,7 @@ public class BinarySearch {
 //        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
         int[] nums2 = new int[1000000];
         for (int i = 0; i < 1000000; i++) {
-            nums2[i] = i+1;
+            nums2[i] = i + 1;
         }
         System.out.println(nums2[499999]);
         System.out.println(new BinarySearch().binarySearch(nums2, 1));
@@ -23,8 +23,9 @@ public class BinarySearch {
         int low = 0;
         int high = nums.length - 1;
         while (low <= high) {
-            int mid = (low + high) / 2;
-            System.out.println(mid+":"+nums[mid]);
+//            int mid = (low + high) / 2;
+            int mid = low + (high - low) / 2;
+            System.out.println(mid + ":" + nums[mid]);
             if (target == nums[mid]) {
                 return mid;
             } else if (target > nums[mid]) {

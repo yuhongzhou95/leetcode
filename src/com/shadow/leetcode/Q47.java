a9e3604184ef26461381f8ad63e4afb57465bfc7
yuhongzhou95 <yuhongzhou95@vip.qq.com>
2020-08-10 10:47:55 +0800

    47. 全排列 II-2

--- src/com/shadow/leetcode/Q47.java
+++ src/com/shadow/leetcode/Q47.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 47. 全排列 II
+ * 第一次刷题：2020年7月20日
+ * 第二次刷题：2020年8月10日09:20:42
+ */
 public class Q47 {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 1, 2};
@@ -12,6 +17,7 @@ public class Q47 {
 
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> resultList = new ArrayList<>();
+        // 排序（升序或者降序都可以），排序是剪枝的前提
         Arrays.sort(nums);
         backtrack(resultList, new ArrayList<Integer>(), nums, new boolean[nums.length]);
         return resultList;
@@ -23,9 +29,12 @@ public class Q47 {
             return;
         }
         for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
             // i>0是为了保证nums[i-1]有意义
-            // used[i-1]是因为在回退的过程中刚刚被撤销选择
-            if (used[i] || i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+            // 为了防止相同的元素在回退的时候再次被选择上
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
                 continue;
             }
             list.add(nums[i]);
